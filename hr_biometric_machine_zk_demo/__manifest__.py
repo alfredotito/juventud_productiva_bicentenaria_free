9c85409802fc8e1344c7be7f1ac4e4f57e7af51d
jonathan <jon.rey8995@gmail.com>
2019-09-06 13:30:15 -0400

    index para modulo demo de zkteco

--- hr_biometric_machine_zk_demo/__manifest__.py
+++ hr_biometric_machine_zk_demo/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    "name" : "ZK Biometric Device Integration Kware (ZKTECO)",
+    "name" : "ZK Biometric Device Integration Kware (ZKTECO) Demo",
     "version" : "1.0",
     "author" : "JUVENTUD PRODUCTIVA VENEZOLANA",
     "category" : "HR",
     "website" : "https://www.youtube.com/channel/UCTj66IUz5M-QV15Mtbx_7yg",
-    "description": "Module for the integration between ZK Biometric Machines and Odoo.",
+    "description": "Module for the connection between odoo and zkteco devices for the control of employee assistance. This module is a demo version to test the compatibility of your device with our module.d Odoo.",
     'license': 'AGPL-3',
     "depends" : ["base","hr"],
     "data" : [
