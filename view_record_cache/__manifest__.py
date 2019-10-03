e8cac6248a37e9ffa388ce700fdbb9f03d7a1fbb
jonathan <jon.rey8995@gmail.com>
2019-10-03 19:21:44 -0400

    modulo cache

---
+++ view_record_cache/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Ir Ui View Cache",
+
+    'summary': """
+       Use this module to accelerate the load""",
+
+    'description': """
+        This module saves the requests of the ir.ui.view object record
+<<<<<<< HEAD
+        s in the cache which is a high speed data storage layer v12
+=======
+        s in the cache which is a high speed data storage layer
+>>>>>>> 869b4b4d5f34ccc0c05adf983e55e052bb87de9b
+    """,
+
+    'author': "JUVENTUD PRODUCTIVA VENEZOLANA",
+    'website': "http://www.juventudproductivabicentenaria.com",
+    'category': 'Performance',
+    'license': 'AGPL-3',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv'
+    ],
+    'demo': [],
+    'images': ['static/images/view_cache_screenshot.png'],
+    'application': True,
+    'installable': True,
+   
+}
