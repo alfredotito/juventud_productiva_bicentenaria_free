74bd363e8328479cb6cbee763b4b3ccbd0c7ca52
Felipe Villamizar <felipeodoo@gmail.com>
2019-10-19 12:39:57 -0400

    [MOD] manifest to openerp

--- resolved_queries/__openerp__.py
+++ resolved_queries/__openerp__.py
@@ -11,17 +11,10 @@
 
     'author': "JUVENTUD PRODUCTIVA BICENTENARIA",
     'website': "http://www.juventudproductivabicentenaria.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Extra Tool',
     'version': '0.1',
     'license': 'AGPL-3',
-    # any module necessary for this one to work correctly
     'depends': ['base'],
-
-    # always loaded
     'data': [
         'security/query_security.xml',
         'views/resolved_queries.xml',
