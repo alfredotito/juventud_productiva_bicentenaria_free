9f573bc1042960d3f7fba482ce519f51bbcb3e33
Felipe Villamizar <felipeodoo@gmail.com>
2019-10-19 19:36:15 -0400

    [ADD licence

--- resolved_queries/__manifest__.py
+++ resolved_queries/__manifest__.py
@@ -17,7 +17,7 @@
     # for the full list
     'category': 'Extra Tool',
     'version': '0.1',
-
+    'license': 'AGPL-3',
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
